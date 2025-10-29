6f9eb2581b433ecbd0fd26d5178e499d1cb752bf
random1531 <loic.raval@gmail.com>
2025-10-29 19:48:34 +0100

    sd

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,75 @@
 "use client";
-import Card from "../components/main/card/card"
-import Data from "../data/receip.json"
-import Filter from "../components/main/filter/filter"
-import Link from "next/link"
-
+import Card from "../components/main/card/card";
+import Data from "../data/receip.json";
+import Filter from "../components/main/filter/filter";
+import Link from "next/link";
+import { useState } from "react";
 export default function Home() {
-const Appareil = Data.map(item => item.appliance)
-const Ustensiles = Data.flatMap(item => item.ustensils)
-const Ingredients = Data.flatMap(item=> item.ingredients.map((i=>i.ingredient)))
-const uniqueIngredients = [...new Set(Ingredients)]
-const uniqueUstensiles = [...new Set(Ustensiles)]
-const uniqueAppareil = [...new Set(Appareil)]
-console.log(uniqueIngredients)
+  const [data, setdata] = useState(Data);
+  const originalData = Data;
+  const Appareil = originalData.map((item) => item.appliance);
+  const Ustensiles = originalData.flatMap((item) => item.ustensils);
+  const Ingredients = originalData.flatMap((item) =>
+    item.ingredients.map((i) => i.ingredient)
+  );
+  const uniqueIngredients = [...new Set(Ingredients)];
+  const uniqueUstensiles = [...new Set(Ustensiles)];
+  const uniqueAppareil = [...new Set(Appareil)];
+  console.log(data);
 
-  return (<>
-      <div className="new">
-        <Filter ValueFilter={uniqueAppareil} />
-         <Filter ValueFilter={uniqueUstensiles} />
-          <Filter ValueFilter={uniqueIngredients} />
+  const resetFilters = () => {
+    setdata(originalData);
+  };
+
+  return (
+    <main>
+      <div className="filterMain">
+        <div className="new">
+          <Filter
+            ValueFilter={uniqueAppareil}
+            OnselectFilter={(item) =>
+              setdata(originalData.filter((ite) => ite.appliance === item))
+            }
+            OnResetFilter={resetFilters}
+          />
+          <Filter
+            ValueFilter={uniqueUstensiles}
+            OnselectFilter={(item) =>
+              setdata(
+                originalData.filter((ite) => ite.ustensils.includes(item))
+              )
+            }
+            OnResetFilter={resetFilters}
+          />
+          <Filter
+            ValueFilter={uniqueIngredients}
+            OnselectFilter={(item) =>
+              setdata(
+                originalData.filter((ite) =>
+                  ite.ingredients.some((ing) => ing.ingredient === item)
+                )
+              )
+            }
+            OnResetFilter={resetFilters}
+          />
+        </div>
+        <div>
+          <span>{data.length} recettes</span>
+        </div>
       </div>
-    <div className="cardContente">
-      {Data.map((e) => (
-        <Link key={e.slug}  href={`receipt/${e.slug}`}>
-        <Card 
-        time={e.time} 
-        picture={e.image} 
-        namereceipt={e.name} 
-        ingredient={e.ingredients}
-        description={e.description} />
-        </Link>
-      ))}
-    </div>
-      </>
+      <div className="cardContente">
+        {data.map((e) => (
+          <Link key={e.slug} href={`receipt/${e.slug}`}>
+            <Card
+              time={e.time}
+              picture={e.image}
+              namereceipt={e.name}
+              ingredient={e.ingredients}
+              description={e.description}
+            />
+          </Link>
+        ))}
+      </div>
+    </main>
   );
 }
--- src/components/main/filter/filter.jsx
+++ src/components/main/filter/filter.jsx
@@ -1,27 +1,54 @@
-import "./filter.css"
+import "./filter.css";
 import { useState } from "react";
-export default function filter({ ValueFilter}) {
-    const [isOpen, setIsOpen] = useState(false)
-    const [tsest,settesest]=useState("")
-    const [selected,setselected]=useState("")
-    const ValuFiltered = ValueFilter.filter(item=> item.toLowerCase().includes(tsest.toLowerCase()))
-    const handleItemClick = (item)=>{setselected(item)}
-    return (
-        <div className={isOpen ? "selectFilterMain2" : "selectFilterMain"}>
-            <div onClick={() => setIsOpen(!isOpen)} className="headerFilter">
-                <span>Ingrédients</span>
-                <span >v</span>
-            </div>
-            {isOpen && (
-                <div>
-                        <input type="search" onChange={(e)=> settesest(e.target.value)}/>
-                    <ul>
-                        {ValuFiltered.map((item) =>
-                            <li onClick={() => handleItemClick(item)} className={selected === item ? "yellow":" "} key={item}>{item}</li>)}
-                    </ul>
-                </div>
-                
-            )}
+export default function filter({ ValueFilter, OnselectFilter, OnResetFilter }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [tsest, settesest] = useState("");
+  const [selected, setselected] = useState("");
+  const [selectedValue, setselectedValue] = useState("");
+  const ValuFiltered = ValueFilter.filter((item) =>
+    item.toLowerCase().includes(tsest.toLowerCase())
+  );
+  const handleItemClick = (item) => {
+    setselected(item);
+    OnselectFilter(item);
+    setselectedValue(item);
+  };
+
+  const handleunselect = () => {
+    setselected("");
+    setselectedValue("");
+    OnResetFilter(); 
+  };
+
+  return (
+    <div>
+      <div className={isOpen ? "selectFilterMain2" : "selectFilterMain"}>
+        <div onClick={() => setIsOpen(!isOpen)} className="headerFilter">
+          <span>Ingrédients</span>
+          <span>v</span>
         </div>
-    )
+        {isOpen && (
+          <div>
+            <input type="search" onChange={(e) => settesest(e.target.value)} />
+            <ul>
+              {ValuFiltered.map((item) => (
+                <li
+                  onClick={() => handleItemClick(item) | setIsOpen(!isOpen)}
+                  className={selected === item ? "yellow" : " "}
+                  key={item}
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      {selected && (
+        <p className="selected" onClick={() => handleunselect()}>
+          {selectedValue}{" "}
+        </p>
+      )}
+    </div>
+  );
 }
